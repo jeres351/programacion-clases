6472dda601be087f138329156531059714e8fbad
Jorge-ortiz <jorgeortizcastillo2020@gmail.com>
2026-09-20 00:39:07 -0300

    Se cambiaron parametros en "Inventario.sql"

--- datos/sql/inventario.sql
+++ datos/sql/inventario.sql
@@ -1,73 +1,118 @@
-# programacion-clases
-tarea grupo
+CREATE DATABASE pyme;
 
-create database pyme;
-use pyme;
+USE pyme;
+
 CREATE TABLE direccion (
-    id_direccion   INT AUTO_INCREMENT PRIMARY KEY,
-    numero_lugar   INT,
-    calle          VARCHAR(100) NOT NULL,
-    comuna         VARCHAR(100) NOT NULL,
-    region         VARCHAR(100) NOT NULL
-);
+    id_direccion INT AUTO_INCREMENT,
+    numero_lugar INT         NOT NULL,
+    calle        VARCHAR(100) NOT NULL,
+    comuna       VARCHAR(100) NOT NULL,
+    region       VARCHAR(100) NOT NULL,
 
-CREATE TABLE categoria (
-    id_categoria   INT AUTO_INCREMENT PRIMARY KEY,
-    nombre         VARCHAR(100) NOT NULL,
-    descripcion    VARCHAR(255)
-);
+    CONSTRAINT pk_direccion 
+    PRIMARY KEY (id_direccion)
+) COMMENT = 'Tabla para registrar las Direcciones';
 
-CREATE TABLE producto (
-    gtin                 VARCHAR(14) PRIMARY KEY,
-    nombre_producto      VARCHAR(150) NOT NULL,
-    descripcion_producto VARCHAR(255),
-    precio_compra        FLOAT
-);
 
 CREATE TABLE proveedor (
-    rut            VARCHAR(12) PRIMARY KEY,
-    nombre         VARCHAR(150) NOT NULL,
-    correo         VARCHAR(150),
-    id_direccion   INT,
+    rut          VARCHAR(15) NOT NULL,
+    nombre       VARCHAR(30),
+    correo       VARCHAR(50),
+    id_direccion INT NOT NULL,
+
+    CONSTRAINT pk_proveedor 
+    PRIMARY KEY (rut),
+    
+    CONSTRAINT fk_p_direccion
     FOREIGN KEY (id_direccion) REFERENCES direccion(id_direccion)
-);
+) COMMENT = 'Esta tabla sirve para que se registre el proveedor';
+
+
+CREATE TABLE categoria(
+    id_categoria          INT AUTO_INCREMENT,
+    nombre                VARCHAR(35) NOT NULL,
+    descripcion_categoria TEXT NULL,
+
+    CONSTRAINT pk_categoria 
+    PRIMARY KEY (id_categoria)
+) COMMENT = 'Esta tabla sirve para el registro de las Categorias, junto con una descripcion general';
+
+CREATE TABLE producto (
+    gtin                 VARCHAR(14),  
+    nombre               VARCHAR(50) NOT NULL,
+    descripcion_producto TEXT NULL,
+    precio_compra        DECIMAL(10,2),
+    
+
+    CONSTRAINT pk_producto 
+    PRIMARY KEY (gtin)
+) COMMENT = 'Esta tabla es para registrar los Productos';
 
 CREATE TABLE almacen (
-    id_almacen     INT AUTO_INCREMENT PRIMARY KEY,
-    nombre         VARCHAR(100) NOT NULL,
-    stock          FLOAT,
-    id_direccion   INT UNIQUE,
-    FOREIGN KEY (id_direccion) REFERENCES direccion(id_direccion)
-);
-
-CREATE TABLE direccion_proveedor (
-    id_direccion_proveedor INT AUTO_INCREMENT PRIMARY KEY,
-    rut_proveedor  VARCHAR(12) NOT NULL,
+    id_almacen     INT AUTO_INCREMENT,
+    nombre_almacen VARCHAR(50),
     id_direccion   INT NOT NULL,
-    FOREIGN KEY (rut_proveedor) REFERENCES proveedor(rut),
+
+    CONSTRAINT pk_almacen
+    PRIMARY KEY (id_almacen), 
+
+    CONSTRAINT fk_a_direccion
     FOREIGN KEY (id_direccion) REFERENCES direccion(id_direccion)
-);
+) COMMENT = 'Esta tabla es para registrar los Almacenes';
 
-CREATE TABLE proveedores_producto (
-    id_proveedor_productos INT AUTO_INCREMENT PRIMARY KEY,
-    rut_proveedor  VARCHAR(12) NOT NULL,
-    gtin           VARCHAR(14) NOT NULL,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE proveedor_producto(
+    rut_proveedor         VARCHAR(15) NOT NULL,
+    gtin_producto         VARCHAR(14) NOT NULL,
+
+    CONSTRAINT pk_proveedor_producto 
+    PRIMARY KEY (rut_proveedor, gtin_producto),
+
+    CONSTRAINT fk_pp_proveedor 
     FOREIGN KEY (rut_proveedor) REFERENCES proveedor(rut),
-    FOREIGN KEY (gtin) REFERENCES producto(gtin)
-);
+    
+    CONSTRAINT fk_pp_producto 
+    FOREIGN KEY (gtin_producto) REFERENCES producto(gtin)
+) COMMENT = 'Esta tabla es para poder saber de que proveedor pertenece cada producto';
 
-CREATE TABLE producto_almacen (
-    id_producto_almacen INT AUTO_INCREMENT PRIMARY KEY,
-    id_almacen     INT NOT NULL,
-    gtin           VARCHAR(14) NOT NULL,
-    FOREIGN KEY (id_almacen) REFERENCES almacen(id_almacen),
-    FOREIGN KEY (gtin) REFERENCES producto(gtin)
-);
+CREATE TABLE categoria_producto(
+    id_categoria          INT NOT NULL,
+    gtin_producto         VARCHAR(14) NOT NULL,
 
-CREATE TABLE categoria_producto (
-    id_categoria_producto INT AUTO_INCREMENT PRIMARY KEY,
-    id_categoria   INT NOT NULL,
-    gtin           VARCHAR(14) NOT NULL,
+    CONSTRAINT pk_categoria_producto 
+    PRIMARY KEY (id_categoria, gtin_producto),
+
+    CONSTRAINT fk_cp_categoria
     FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
-    FOREIGN KEY (gtin) REFERENCES producto(gtin)
-);
+
+    CONSTRAINT fk_cp_producto
+    FOREIGN KEY (gtin_producto) REFERENCES producto(gtin)
+) COMMENT = 'Esta tabla es para poder saber cuantas Categorias tiene Cada Producto';
+
+CREATE TABLE producto_almacen(
+    gtin_producto       VARCHAR(14) NOT NULL,
+    id_almacen          INT NOT NULL,
+    stock               INT NOT NULL,
+
+    CONSTRAINT pk_producto_almacen
+    PRIMARY KEY (gtin_producto, id_almacen),
+
+    CONSTRAINT fk_pa_producto
+    FOREIGN KEY (gtin_producto) REFERENCES producto(gtin),
+
+    CONSTRAINT fk_pa_almacen
+    FOREIGN KEY (id_almacen) REFERENCES almacen(id_almacen)
+) COMMENT = 'Esta tabla es para poder saber cuantos productos esta en cada almacen';
